9f6e2787c9aa92e75e49d0bb07765ae7859fa65a
imakshat30 <kaushikakshat007@gmail.com>
2019-12-18 23:43:39 +0530

    Chapter-2 Commit-1

---
+++ Chapter-2/strings.py
@@ -0,0 +1,10 @@
+#strings
+#collection of characters inside single quotes or double quotes
+
+first_name = "Akshat"
+last_name = "Kaushik"                                                        
+full_name = first_name + " " + last_name
+print(full_name)
+
+print(full_name + 3) #this will always give an error as a string cannot be related or added with a number
+print(full_name * 3)# In thi the strinf fullname will be printed 3 times
---
+++ Chapter-2/user_input.py
@@ -0,0 +1,9 @@
+# USER INPUT
+# INPUT FUNCTION
+name = input("Type Your Name : ")
+print("HELLO " + name)
+
+#STRING
+age = input("What is your AGE? ")
+# 24, "24"
+print("Your Age is " + age)
