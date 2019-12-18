e4f706b698fd4999943929d07883428bb82280e5
imakshat30 <kaushikakshat007@gmail.com>
2019-12-18 14:08:24 +0530

    Chapter-1 Commit-2

---
+++ Chapter-1/variable.py
@@ -0,0 +1,20 @@
+number1 = 2
+print(number1)
+number1 = 4
+print(number1)
+# STRING, NUMBER
+
+name = "AKSHAT"
+print(name) 
+name = 123
+print(name)
+
+
+# RULES FOR NAMING A VARIABLE
+# 1. Variable name cannot start with a number i.e 1number = 4
+# 2. You can start the variable name by any alphabet , _ i.e _name = "AKSHAT"
+# 3. No special symbols can be used in a variable name such as $ 
+
+# *********************NAMING CONVENTIONS FOR VARIABLES*****************
+# user_one_name = "AKSHAT" this is snake case writing
+#userOneName = "AKSHAT" this is camel case writing
